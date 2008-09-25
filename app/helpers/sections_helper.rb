9e81eb93f93a59aad4a73f9f75ad196b70828361
Sam Pierson <sam@sampierson.com>
2008-09-24 17:50:24 -0700

    Changed section title [Edit] button to a pen image.

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -4,8 +4,8 @@ module SectionsHelper
     id = "section#{section.id}_in_place_editor"
     url = set_section_title_path :id => section, :authenticity_token => form_authenticity_token
     edit_button_id = "section_#{section.id}_title_edit"
-    "<span class=in_place_editor_field id=#{id}>#{section.title}</span>" +
-    "<button id=#{edit_button_id}>edit</button>" +
+    "<span class=in_place_editor_field id=#{id}>#{section.title}</span>&nbsp;" +
+    "<span id=#{edit_button_id}>#{image_tag('pen.png')}</span>" +
     "<script type='text/javascript'>
     //<![CDATA[
     new Ajax.InPlaceEditor(#{id}, '#{url}', {externalControl: '#{edit_button_id}', externalControlOnly:true})
